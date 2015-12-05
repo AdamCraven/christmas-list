f4042bbfe442a92548bab074161868bf0c710b45
Adam Craven <git@adamcraven.me>
2015-12-05 17:46:09 +0000

    tidying scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,13 @@
 var gulp = require('gulp');  //This tells node to look into node_modules folder for the package gulp, when it is found, it is assigned to the variable
-
-gulp.task('hello', function() {
-	console.log('Hello Andrew');  //I used this as a test to see if my gulpfile was working.. it was!!
-});
-
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function() {
-	return gulp.src('app/scss/christmasStyle.scss') //I think that this locates the sass file to change
-	.pipe(sass()) //This uses gulp-sass and converts it to CSS
-	.pipe(gulp.dest('app')); // This places it in the app folder (maybe I shoud create a CSS folder)	
+	gulp.src('./app/styles/*.scss') //I think that this locates the sass file to change
+  	.pipe(sass()) //This uses gulp-sass and converts it to CSS
+  	.pipe(gulp.dest('./app/styles')); // This places it in the app folder (maybe I shoud create a CSS folder)
 });
 
 gulp.task('watch', function() {
-	gulp.watch('app/scss/christmasStyle.scss', ['sass']);
+	return gulp.watch('app/scss/main.scss', ['sass']);
 });
