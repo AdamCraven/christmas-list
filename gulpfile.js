faa55887549370d6cb281e34d2ac1dfd6917b4bc
Adam Craven <git@adamcraven.me>
2015-12-05 17:59:03 +0000

    auto prefixer, seperated css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');  //This tells node to look into node_modules folder for the package gulp, when it is found, it is assigned to the variable
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
 	gulp.src('./app/styles/*.scss') //I think that this locates the sass file to change
   	.pipe(sass()) //This uses gulp-sass and converts it to CSS
+    .pipe(autoprefixer({browsers: ['last 2 versions']}))
   	.pipe(gulp.dest('./app/styles')); // This places it in the app folder (maybe I shoud create a CSS folder)
 });
 
-gulp.task('watch', function() {
-	return gulp.watch('app/scss/main.scss', ['sass']);
+gulp.task('watch', ['sass'], function() {
+	return gulp.watch('app/styles/**/*.scss', ['sass']);
 });
